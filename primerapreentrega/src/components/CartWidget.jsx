255d3e0c14067ee6f41d991beaad42caa3db01b5
LucasSpinelli33 <luqui.spinelli@gmail.com>
2024-10-23 14:04:41 -0300

    segunda carga

--- primerapreentrega/src/components/CartWidget.jsx
+++ primerapreentrega/src/components/CartWidget.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import carrito from '../assets/img/carrito.png';
+import carrito from '../assets/img/carrito.png'; 
 
-function CartWidget () {
+function CartWidget() {
     return (
-       <>
-          <CartWidget>
-            <a href=""><img src="{carrito}" alt="" /></a>
-          </CartWidget>
-       </> 
-    )
+        <div style={{ position: 'relative' }}>
+            <img src={carrito} alt="Carrito" style={{ height: '30px' }} />
+            <div style={{
+                position: 'absolute',
+                top: '0',
+                right: '0',
+                backgroundColor: 'red',
+                color: 'white',
+                borderRadius: '50%',
+                padding: '2px 6px',
+                fontSize: '12px',
+                textAlign: 'center', // Centrar el texto
+                width: '20px', // Ancho fijo para el círculo
+            }}>
+                3 
+            </div>
+        </div>
+    );
 }
 
-export default CartWidget
+export default CartWidget;
